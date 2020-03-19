55546b28ee794040bb287127bfd33c59a6f61ecc
Noam Buckman macbook <nbuckman@mit.edu>
2020-03-19 17:06:37 -0400

    decay

--- src/MPC_Casadi.py
+++ src/MPC_Casadi.py
@@ -121,6 +121,7 @@ class MPC:
         self.change_u_delta = cas.sumsqr(U[0,1:N-1] - U[0,0:N-2])
         self.change_u_v = cas.sumsqr(U[1,1:N-1] - U[1,0:N-2])
         self.x_cost = cas.sumsqr(X[0,:])
+        self.x_cost = cas.sum([.95**k * X[0,k]**2 for k in range(X.shape[0])])
         self.x_dot_cost = cas.sumsqr(X[4, :] * cas.cos(X[2,:]))
 
     def total_cost(self):
