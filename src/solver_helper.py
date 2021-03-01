4881cbf2824f5292ef69d0184c9daa90214c5347
Noam Buckman <nbuckman@mit.edu>
2021-03-01 00:26:42 -0500

    IDM baseline working

--- src/solver_helper.py
+++ src/solver_helper.py
@@ -630,5 +630,139 @@ def IDM_trajectory_prediction(veh, N, X_0, X_lead=None, desired_speed=None, idm_
     # print("x", x)
     # print("x_des", x_des)
     return U_ego, x, x_des
-    X_ego[:, t + 1] = x[:, 1]
-    X_des[:, t:t + 2] = x_des  # update two points at a time since it computes both x_d(t) and x_d(t+1)
+
+
+def MOBIL_lanechange(driver_x0, driver_veh, all_other_x0, all_other_veh, world, MOBIL_params=None, IDM_params=None):
+    ''' MOBIL lane changing rules '''
+
+    default_MOBIL_params = {
+        "politeness_factor": 0.5,
+        "changing_threshold": 0.1,
+        "maximum_safe_deceleration": 4,
+        "bias_for_right_lane": 0.3
+    }
+    if MOBIL_params:
+        for param in MOBIL_params:
+            try:
+                default_MOBIL_params[param] = MOBIL_params[param]
+            except KeyError:
+                raise Exception("Key Error:  Check if MOBIL Param is correct")
+
+    p = default_MOBIL_params["politeness_factor"]
+    a_thr = default_MOBIL_params["changing_threshold"]
+    b_safe = default_MOBIL_params["maximum_safe_deceleration"]
+    a_bias = default_MOBIL_params["bias_for_right_lane"]
+
+    driver_old_lane = world.get_lane_from_x0(driver_x0)
+    driver_new_lanes = [li for li in range(world.n_lanes) if li != driver_old_lane]
+
+    best_new_lane = None
+    for new_lane in driver_new_lanes:
+
+        new_follower_idx = get_prev_vehicle_lane(driver_x0, all_other_x0, new_lane, world)
+        new_leader_idx = get_next_vehicle_lane(driver_x0, all_other_x0, new_lane, world)
+
+        old_follower_idx = get_prev_vehicle_lane(driver_x0, all_other_x0, driver_old_lane, world)
+        old_leader_idx = get_next_vehicle_lane(driver_x0, all_other_x0, driver_old_lane, world)
+
+        # Calculate the new acceleration of the new follower
+
+        follower_lead_pairs = {
+            "newfollower_after": (new_follower_idx, -1),
+            "newfollower_before": (new_follower_idx, new_leader_idx),
+            "oldfollower_before": (old_follower_idx, -1),
+            "oldfollower_after": (old_follower_idx, old_leader_idx),
+            "driver_after": (-1, new_leader_idx),
+            "driver_before": (-1, old_leader_idx)
+        }
+
+        accel = {}
+        lane_gap = True
+        for key, (follower_idx, lead_idx) in follower_lead_pairs.items():
+            if follower_idx is None:
+                # no follower, just set acceleration = 0 for before & after
+                accel[key] = 0
+                continue
+            elif follower_idx == -1:
+                follower_x0 = driver_x0
+                follower_veh = driver_veh
+            else:
+                follower_x0 = all_other_x0[follower_idx]
+                follower_veh = all_other_veh[follower_idx]
+
+            current_speed = follower_x0[4] * np.cos(follower_x0[2])
+            desired_speed = follower_veh.max_v
+
+            if lead_idx is None:  # no cars ahead
+                bumper_distance = 999999 - follower_x0[0] - follower_veh.L
+                lead_velocity = 999999
+            elif lead_idx == -1:  # lead vehicle is the driver vehicle
+                bumper_distance = driver_x0[0] - follower_x0[0] - follower_veh.L
+                lead_velocity = driver_x0[4] * np.cos(driver_x0[2])
+            else:
+                bumper_distance = all_other_x0[lead_idx][0] - follower_x0[0] - follower_veh.L
+                lead_velocity = all_other_x0[lead_idx][4] * np.cos(all_other_x0[lead_idx][2])
+            if lead_idx == -1:
+                print(key, follower_idx, lead_idx)
+                print(bumper_distance)
+            if bumper_distance < 0:
+                lane_gap = False  #checks if there is even a gap between lead vehicle in next lane
+            a_follower = IDM_acceleration(bumper_distance, lead_velocity, current_speed, desired_speed, IDM_params)
+            accel[key] = a_follower
+
+        safety_criteria = accel["newfollower_after"] >= -b_safe
+
+        driver_incentive = accel["driver_after"] - accel["driver_before"]
+        new_follower_incentive = accel["newfollower_after"] - accel["newfollower_before"]
+        old_follower_incentive = accel["oldfollower_after"] - accel["oldfollower_before"]
+
+        incentive_criteria = (driver_incentive + p * (new_follower_incentive + old_follower_incentive)) >= (a_thr)
+
+        if incentive_criteria and safety_criteria and lane_gap:
+            best_new_lane = new_lane
+
+    return best_new_lane, accel
+
+
+def get_lead_vehicle(x0, other_x0, world):
+    n_other = len(other_x0)
+    ego_lane = world.get_lane_from_x0(x0)
+
+    veh_same_lane = [world.get_lane_from_x0(other_x0[i]) == ego_lane for i in range(n_other)]
+    veh_in_front = [(other_x0[i][0] - x0[0]) > 0 for i in range(n_other)]
+
+    vehicles_sorted = np.argsort([(other_x0[i][0] - x0[0])**2 for i in range(n_other)])
+    vehicles_sorted_valid = [idx for idx in vehicles_sorted if veh_same_lane[idx] and veh_in_front[idx]]
+
+    if len(vehicles_sorted_valid) > 0:
+        return vehicles_sorted_valid[0]
+    else:
+        return None
+
+
+def get_next_vehicle_lane(x0, all_other_x0, lane, world):
+    ''' get next vehicle in a lane '''
+
+    idx_in_lane = [world.get_lane_from_x0(all_other_x0[idx]) == lane for idx in range(len(all_other_x0))]
+    idx_forward = [all_other_x0[idx][0] - x0[0] > 0 for idx in range(len(all_other_x0))]
+    idx_dist_sorted = np.argsort([np.abs(all_other_x0[idx][0] - x0[0]) for idx in range(len(all_other_x0))])
+
+    idx_dist_sorted_valid = [idx for idx in idx_dist_sorted if idx_in_lane[idx] and idx_forward[idx]]
+    if len(idx_dist_sorted_valid) > 0:
+        return idx_dist_sorted_valid[0]
+    else:
+        return None
+
+
+def get_prev_vehicle_lane(x0, all_other_x0, lane, world):
+    ''' get previous vehicle in a lane '''
+
+    idx_in_lane = [world.get_lane_from_x0(all_other_x0[idx]) == lane for idx in range(len(all_other_x0))]
+    idx_forward = [all_other_x0[idx][0] - x0[0] < 0 for idx in range(len(all_other_x0))]
+    idx_dist_sorted = np.argsort([np.abs(all_other_x0[idx][0] - x0[0]) for idx in range(len(all_other_x0))])
+
+    idx_dist_sorted_valid = [idx for idx in idx_dist_sorted if idx_in_lane[idx] and idx_forward[idx]]
+    if len(idx_dist_sorted_valid) > 0:
+        return idx_dist_sorted_valid[0]
+    else:
+        return None
--- src/traffic_world.py
+++ src/traffic_world.py
@@ -1,48 +1,65 @@
-
 import numpy as np
+
 
 class TrafficWorld():
     def __init__(self, n_lanes_right, n_lanes_left, x_max=np.infty, lane_width=3.7):
-        
-        self.lane_width = lane_width #m avg lane width
+
+        self.lane_width = lane_width  #m avg lane width
         self.n_lanes_right = n_lanes_right
         self.n_lanes_left = n_lanes_left
         self.n_lanes = self.n_lanes_left + self.n_lanes_right
 
-        self.grass_width = self.lane_width/2.0
+        self.grass_width = self.lane_width / 2.0
 
         self.total_height = self.get_window_height()
-        
-        self.y_min = -0.5*self.lane_width - self.grass_width
-        self.y_max = (- 0.5 + self.n_lanes) * self.lane_width + self.grass_width
-        self.x_max = x_max
-        self.x_min = - self.lane_width # NO REAL GOOD REASON
 
+        self.y_min = -0.5 * self.lane_width - self.grass_width
+        self.y_max = (-0.5 + self.n_lanes) * self.lane_width + self.grass_width
+        self.x_max = x_max
+        self.x_min = -self.lane_width  # NO REAL GOOD REASON
 
     def get_window_height(self):
-        return 2*self.grass_width + self.n_lanes * self.lane_width 
+        return 2 * self.grass_width + self.n_lanes * self.lane_width
 
     def get_lane_centerline_y(self, lane_number, right_direction=True):
         # count starts at 0
         if right_direction:
-            centerline_y = lane_number*self.lane_width
+            centerline_y = lane_number * self.lane_width
         else:
-            centerline_y = (self.n_lanes_right + 1)*self.lane_width + lane_number*self.lane_width
+            centerline_y = (self.n_lanes_right + 1) * self.lane_width + lane_number * self.lane_width
 
         return centerline_y
 
     def get_bottom_grass_y(self):
         y_bottom = self.y_min
         y_top = self.y_min + self.grass_width
-        y_center = self.y_min + self.grass_width/2.0
+        y_center = self.y_min + self.grass_width / 2.0
         return y_bottom, y_center, y_top
 
     def get_top_grass_y(self):
         y_top = self.y_max
         y_bottom = self.y_max - self.grass_width
-        y_center = self.y_max - self.grass_width/2.0
-        return y_bottom, y_center, y_top    
+        y_center = self.y_max - self.grass_width / 2.0
+        return y_bottom, y_center, y_top
 
+    def get_lane_from_x0(self, x0, right_direction=True):
+        ''' return the lane number from vehicle's position '''
+
+        y = x0[1]
+        min_lane = -1
+        min_dist = np.infty
+
+        if not right_direction:
+            raise Exception("We haven't implemented left lanes yet")
+        else:
+            for li in range(self.n_lanes):
+                center_y = self.get_lane_centerline_y(li, right_direction)
+                y_dist = np.abs(center_y - y)
+                if y_dist <= min_dist:
+                    min_dist = y_dist
+                    min_lane = li
+
+        return min_lane
 
 
 ######################################################3
