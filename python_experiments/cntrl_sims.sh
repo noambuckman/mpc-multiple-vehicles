cdc2e98516171a83193582849984e5d17cdf7253
Noam Buckman <nbuckman@mit.edu>
2021-03-03 18:31:03 -0500

    fixed log

--- python_experiments/cntrl_sims.sh
+++ python_experiments/cntrl_sims.sh
@@ -2,7 +2,6 @@
 
 #SBATCH --cpus-per-task 8
 #SBATCH -a 1-2
-echo $1
 
 # echo $(date +%H%m%s)
 printf -v date '%(%Y-%m-%d)T\n' -1 
@@ -30,7 +29,7 @@ CAR_DENSITY=3000
 RANDOM_SVO=1
 SVO_THETA=0.0
 
-LOG_SUBDIR='cntrld/cntrld_$1_${SLURM_ARRAY_TASK_ID}_${date}'
+LOG_SUBDIR='cntrld/cntrld_'${SLURM_ARRAY_TASK_ID}_${date}
 echo $LOG_SUBDIR
 
 python src/iterative_best_response.py --seed $SLURM_ARRAY_TASK_ID --log-subdir $LOG_SUBDIR --n-cntrld $N_CNTRLD --n-processors $SLURM_CPUS_PER_TASK --n-mpc $N_MPC --car-density $CAR_DENSITY --n-other $N_OTHER --n-lanes $N_LANES --random-svo $RANDOM_SVO --svo-theta $SVO_THETA --save-ibr $SAVE_IBR
