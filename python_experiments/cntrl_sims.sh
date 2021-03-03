551c8fa20a60887e8913cdeb3e0e919108fd116a
Noam Buckman <nbuckman@mit.edu>
2021-03-03 18:11:00 -0500

    Update

--- python_experiments/cntrl_sims.sh
+++ python_experiments/cntrl_sims.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #SBATCH --cpus-per-task 8
-#SBATCH -a 1-4
+#SBATCH -a 1-2
+echo $1
 
 # echo $(date +%H%m%s)
 printf -v date '%(%Y-%m-%d)T\n' -1 
@@ -15,21 +16,21 @@ conda activate mpc
 export PYTHONPATH=$PYTHONPATH:/home/gridsan/nbuckman/mpc-multiple-vehicles
 cd mpc-multiple-vehicles
 
-N_CNTRLD=2
+N_CNTRLD=3
 
 
 
 N_OTHER=20
 N_LANES=2
-RANDOM_SVO=1
 SAVE_IBR=1
 N_MPC=30
 
 CAR_DENSITY=3000
-SVO_THETA=0.0
-SEED=1
 
-LOG_SUBDIR='cntrld_test' + $SLURM_ARRAY_TASK_ID
+RANDOM_SVO=1
+SVO_THETA=0.0
+
+LOG_SUBDIR='cntrld/cntrld_$1_${SLURM_ARRAY_TASK_ID}_${date}'
 echo $LOG_SUBDIR
 
-# python src/iterative_best_response.py --seed $SLURM_ARRAY_TASK_ID --n-mpc $N_MPC --car-density $CAR_DENSITY --n-other $N_OTHER --n-lanes $N_LANES --n-cntrld $N_CNTRLD --random-svo $RANDOM_SVO --svo-theta $SVO_THETA --log-subdir $LOG_SUBDIR --save-ibr $SAVE_IBR
+python src/iterative_best_response.py --seed $SLURM_ARRAY_TASK_ID --log-subdir $LOG_SUBDIR --n-cntrld $N_CNTRLD --n-processors $SLURM_CPUS_PER_TASK --n-mpc $N_MPC --car-density $CAR_DENSITY --n-other $N_OTHER --n-lanes $N_LANES --random-svo $RANDOM_SVO --svo-theta $SVO_THETA --save-ibr $SAVE_IBR
