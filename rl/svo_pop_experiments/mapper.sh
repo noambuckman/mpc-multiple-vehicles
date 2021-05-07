6e95dc400e6763e5d40faff2d1e2e290600842c0
Noam Buckman <nbuckman@mit.edu>
2021-05-07 16:58:38 -0400

    hardcode experiments

--- rl/svo_pop_experiments/mapper.sh
+++ rl/svo_pop_experiments/mapper.sh
@@ -4,8 +4,8 @@ conda activate mpc
 export PYTHONPATH=$PYTHONPATH:/home/gridsan/nbuckman/mpc-multiple-vehicles
 cd /home/gridsan/nbuckman/mpc-multiple-vehicles/rl/svo_pop_experiments
 
-EXPERIMENT_RANDOM_SEED=$(($SLURM_ARRAY_TASK_ID + $RANDOM))
-echo $1
+EXPERIMENT_RANDOM_SEED=$RANDOM
+INPUT_PARAMS="svo_population_experiments.json"
 echo $LLSUB_RANK 
 echo $LLSUB_SIZE
-python sim_mapper_population.py $LLSUB_RANK $LLSUB_SIZE --input-params $1 --experiment-random-seed $EXPERIMENT_RANDOM_SEED 
+python sim_mapper_population.py $LLSUB_RANK $LLSUB_SIZE --input-params $INPUT_PARAMS --experiment-random-seed $EXPERIMENT_RANDOM_SEED 
