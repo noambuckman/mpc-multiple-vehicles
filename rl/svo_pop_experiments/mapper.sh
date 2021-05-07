bd1fe8d3bd065fa793dfc7a83b0666b62248f75a
Noam Buckman <nbuckman@mit.edu>
2021-05-07 16:49:54 -0400

    Some smaller expeirments

--- rl/svo_pop_experiments/mapper.sh
+++ rl/svo_pop_experiments/mapper.sh
@@ -5,7 +5,7 @@ export PYTHONPATH=$PYTHONPATH:/home/gridsan/nbuckman/mpc-multiple-vehicles
 cd /home/gridsan/nbuckman/mpc-multiple-vehicles
 
 EXPERIMENT_RANDOM_SEED=$(($SLURM_ARRAY_TASK_ID + $RANDOM))
-
+echo $1
 echo $LLSUB_RANK 
 echo $LLSUB_SIZE
-python sim_mapper_population.py $LLSUB_RANK $LLSUB_SIZE --experiment-random-seed EXPERIMENT_RANDOM_SEED 
+python sim_mapper_population.py $LLSUB_RANK $LLSUB_SIZE --input-params $1 --experiment-random-seed $EXPERIMENT_RANDOM_SEED 
