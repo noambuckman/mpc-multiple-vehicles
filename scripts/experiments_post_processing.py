272af5f1af092ad68f77b17bceb13ea082f50581
Noam Buckman <nbuckman@mit.edu>
2022-04-19 13:06:17 -0400

    post processing scripts

---
+++ scripts/experiments_post_processing.py
@@ -0,0 +1,254 @@
+import os, pickle, json, glob, argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+CONV_MS_TO_MPH = 2.23694
+
+
+def post_process_experiments(args):
+    ''' Load the directory and print the'''
+    
+    all_params, all_trajs, all_vehicles, all_dirs, experiment_params = load_experiment_results(args.experiment_dir)
+
+    p_coop_array = np.array([all_params[exi]['p_cooperative'] for exi in range(len(all_params))])
+    distances_array = np.array([all_trajs[exi][:,0, args.n_mpc_end] - all_trajs[exi][:,0,0] for exi in range(len(all_params))])
+    p_distance_dict = {}
+    p_passing_dict = {}
+    for exi in range(distances_array.shape[0]):
+        if p_coop_array[exi] not in p_distance_dict:
+            p_distance_dict[p_coop_array[exi]] = []
+        p_distance_dict[p_coop_array[exi]] += [distances_array[exi]]
+        
+        
+    for p in p_distance_dict:
+        p_distance_dict[p] = np.array(p_distance_dict[p])
+    for p in p_passing_dict:
+        p_passing_dict[p] = np.array(p_passing_dict[p])
+        
+    p_unique = np.array(list(p_distance_dict.keys()))
+    p_unique = np.sort(p_unique)
+
+    params = all_params[0]
+    total_time = params["dt"] * args.n_mpc_end
+    print("Total Sim Time", total_time)
+
+    all_seeds = [param["seed"] for param in all_params]
+    all_densities = [param["car_density"] for param in all_params]
+    all_p_cooperative = [param["p_cooperative"] for param in all_params]
+
+    summarize_sim_params(all_params, all_vehicles)
+
+    total_collisions, total_experiments, percentage_collisions_experiments, collisions_per_km, collisions_per_bkm = get_collision_stats(all_trajs)
+    print("N_collisions: %d,  N_exp: %d, P_collisions: %.2f,  Collisions/Km: %.3e, Collisions/BKm %.1e"%(
+                    total_collisions, total_experiments, percentage_collisions_experiments, collisions_per_km, collisions_per_bkm)) 
+
+
+    if args.analyze_collisions:
+        for exp_i in range(len(all_trajs)):
+            if check_collision_experiment(all_trajs[exp_i]):
+                print(all_dirs[exp_i])
+                print("P", all_params[exp_i]["p_cooperative"])
+                print("Density",  all_params[exp_i]["car_density"])
+                print("Collision Index", get_collision_index(all_trajs[exp_i]))
+                print("Seed", all_params[exp_i]["seed"])    
+
+
+        collision_params = get_collision_parameters(all_params, all_trajs)
+        summarize_sim_params(collision_params, all_vehicles, "collision_params_hist.png")
+
+    if args.animate_by_density:
+        animate_by_density(experiment_params, all_dirs, all_params)
+
+
+
+def load_experiment_results(experiment_dir):
+    ''' Load experiment param and results'''
+    sim_dirs = glob.glob(os.path.join(experiment_dir,'results/*-*'))
+    print("# Sim Dirs: %d"%len(sim_dirs))
+
+    # experiment_params_path = glob.glob('/home/nbuckman/mpc_results/txe_0719/experiment.json')
+    experiment_params_path = os.path.join(experiment_dir, "experiment.json")
+    experiment_params = json.load(open(experiment_params_path,'rb'))
+    
+    all_params = []
+    all_trajs = []
+    all_vehicles = []
+    all_dirs = []
+
+    for sim_dir in sim_dirs:
+        params_path = glob.glob(sim_dir + '/params.json')
+        trajs_path = glob.glob(sim_dir + '/trajectories.npy')
+        vehicles_path = glob.glob(sim_dir + '/other_vehicles.p')
+        
+        if len(trajs_path) > 0:
+            all_params += [json.load(open(params_path[0],'rb'))]
+            all_trajs += [np.load(trajs_path[0])]
+            all_vehicles += [pickle.load(open(vehicles_path[0],'rb'))]
+            all_dirs += [sim_dir]
+    print("# Simulations: %d"%len(all_dirs))
+
+    return all_params, all_trajs, all_vehicles, all_dirs, experiment_params
+
+
+def animate_by_density(experiment_params, all_dirs, all_params):
+    
+    for density in experiment_params['car_density']:
+        vids_to_concat = []
+        for i in range(len(all_dirs)):
+            sim_dir = all_dirs[i]
+            if all_params[i]['car_density'] == density:
+                vid_path = glob.glob(sim_dir + '/vids/*.mp4')
+                
+                if len(vid_path) > 0:
+                    vids_to_concat += [vid_path[0]]
+        output_file_name = concat_vids(vids_to_concat, "all_density%d.mp4"%density)
+        print(output_file_name)
+
+
+
+
+
+def get_collision_stats(all_trajs):
+    ''' Compute collision/km'''
+
+    distances_array = np.array([all_trajs[exi][:,0,args.n_mpc_end] - all_trajs[exi][:,0,0] for exi in range(len(all_trajs))])
+    total_distance_meters = np.sum(distances_array)
+    total_collisions = np.sum([check_collision_experiment(traj) for traj in all_trajs])
+    total_experiments = len(all_trajs)
+    percentage_collisions_experiments = float(total_collisions) / total_experiments
+    collisions_per_km = float(total_collisions) / (total_distance_meters / 1000.0) 
+    collisions_per_bkm = collisions_per_km * (1e9)
+
+
+    return total_collisions, total_experiments, percentage_collisions_experiments, collisions_per_km, collisions_per_bkm
+
+
+def get_collision_parameters(all_params, all_trajs):
+    ''' Return list of parameters corresponding to experiments with collisions'''
+
+    collision_params = [all_params[exp_i] for exp_i in range(len(all_trajs)) if check_collision_experiment(all_trajs[exp_i])]    
+
+    return collision_params
+    
+
+def summarize_sim_params(all_params, all_vehicles, filename="param_summary_hist.png"):
+    ''' Create a histogram of all the params'''
+    
+    fig, axs = plt.subplots(2,2)
+    color=iter(plt.cm.rainbow(np.linspace(0,1,4)))
+    counter = 0
+    
+    for param_key in ["seed", "car_density", "p_cooperative"]:
+        row = counter%2
+        col = counter // 2
+        ax = axs[row, col]
+        all_p_key = [param[param_key] for param in all_params]
+        c = next(color)
+        ax.hist(all_p_key, facecolor=c)
+        ax.set_xlabel(param_key)
+        ax.set_ylabel("# Exp")
+        counter+=1
+        
+    c = next(color)    
+    all_max_speeds = [veh.max_v for vehicles in all_vehicles for veh in vehicles]
+    axs[1,1].hist(all_max_speeds)
+    axs[1,1].set_xlabel('Max Speed')
+    axs[1,1].set_ylabel('# Vehicles')
+    fig.suptitle("Experiment Settings")
+
+    fig.tight_layout()
+    fig.show()
+
+    plt.savefig(filename)
+
+
+def get_collision_index(traj):
+    ''' Check for collision in experiment
+        If coollision occurs, the trajectory will be zeros(1,6)
+    '''
+    collision_array = np.all(traj[:,:,1:] == np.zeros((1, 6, 1)), axis=1) # vehs x time
+    collision_array = np.any(collision_array, axis=0) # time
+    return np.argmax(collision_array)
+
+def check_collision_experiment(traj):
+    ''' Check for collision in experiment
+        If coollision occurs, the trajectory will be zeros(1,6)
+    '''
+    if np.any(np.all(traj[:,:,1:] == np.zeros((1, 6, 1)), axis=1)):
+        return True
+    return False
+
+
+def concat_vids(list_vids_to_concat, output_file, dry_run=False, n_max_vids=16, n_columns=2):
+    ''' Takes a list of videos to concat and adds them to a single video'''
+    
+    n_vids = min(len(list_vids_to_concat), n_max_vids)
+    n_rows = int(np.ceil(n_vids / n_columns))
+
+
+    input_files = list_vids_to_concat[:n_vids]
+    temp_files = []
+
+
+    if os.path.exists(output_file):
+        os.remove(output_file)
+
+    for input_file in input_files:
+        # Crop
+        temp_file = input_file[:-4] + "temp.mp4"
+        crop_command = "ffmpeg -i %s -filter 'crop=iw*.9:ih*.3' %s"%(input_file, temp_file)
+        if not dry_run:
+            os.system(crop_command)
+        temp_files += [temp_file]
+
+
+    all_filter_commands = ""
+    for vint in range(len(input_files)):
+        row = vint//2
+        column = vint%2
+
+        if vint==0:
+            filter_command = "[%d:v]pad=iw*%d:ih*%d[int%d];"%(vint,n_columns, n_rows, vint)
+        else:
+            if column == 1:
+                overlay_x = "W/2"
+            else:
+                overlay_x = "0"
+
+            overlay_y = "%d/%d*H"%((row), n_rows)
+
+            filter_command = "[int%d][%d:v]overlay=%s:%s[int%d];"%(vint-1, vint, overlay_x, overlay_y, vint)
+        all_filter_commands += filter_command
+
+
+    concat_command = "ffmpeg "
+    for vint in range(len(input_files)):
+        concat_command += "-i %s "%temp_files[vint]
+    concat_command += "-filter_complex '%s' "%all_filter_commands[:-1]
+    concat_command += "-map '[int%d]' -c:v libx264 -crf 23 -preset veryfast %s"%(vint, output_file)
+
+
+    # concat_command = "ffmpeg -i %s -i %s -i %s -i %s -filter_complex '[0:v]pad=iw*2:ih*3[int];[int][1:v]overlay=W/2:H/3[vid];[vid][2:v]overlay=0:H/3[vid2]' -map '[vid2]' -c:v libx264 -crf 23 -preset veryfast %s"%(
+    #                     *temp_files, output_file)
+    print(concat_command)
+    if not dry_run:
+        os.system(concat_command)
+        for temp_file in temp_files:
+            if os.path.exists(temp_file):
+                os.remove(temp_file)    
+    
+    return output_file
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("experiment_dir", type=str, help="experiment directory to analyze")
+    parser.add_argument("--n-mpc-end", type=int, default=500, help="experiment directory to analyze")
+
+    parser.add_argument("--analyze-collisions", action="store_true", help="Print out parameters for every simulation with collisions")
+    parser.add_argument("--animate-by-density", action="store_true", help="animate videos and concatenate by SVO")
+    args = parser.parse_args()
+
+    post_process_experiments(args)
--- scripts/post_processing.py
+++ scripts/post_processing.py
@@ -1,21 +1,142 @@
-import argparse
-import os, pickle, json
+import os, pickle, json, glob, argparse
 import numpy as np
-from src.utils.plotting import car_plotting
 import matplotlib.pyplot as plt
+
+from src.utils.plotting.car_plotting import plot_cars, animate, plot_initial_positions
+
+
 def post_process(args):
     ''' Load the directory and print the'''
+    for logdir in args.logdir:
+        print(logdir)
+        try:
+            params, vehicles, world, x0, x, u = load_data_from_logdir(logdir)    
+        except FileNotFoundError as e:
+            print(e)
+            continue
 
-    LOGDIR = args.logdir
-    X0_PATH = os.path.join(LOGDIR, "x0.p")
+        if args.show_initial_positions:
+            plot_initial_positions(logdir, world, vehicles, x0, number_cars_included=10)
+
+        if args.analyze_controls:
+            analyze_controls(logdir, u, x)
+
+
+        if args.animate:
+            animate_cars(args, params, logdir, x, vehicles, world)
+
+
+def load_data_from_logdir(logdir: str):
+    ''' Loads experiment data from logdirectory string'''
+    
+    X0_PATH = os.path.join(logdir, "x0.p")
     x0 = pickle.load(open(X0_PATH, "rb"))
 
-    U_PATH = os.path.join(LOGDIR, "controls.npy")
-    X_PATH = os.path.join(LOGDIR, "trajectories.npy")
+    U_PATH = os.path.join(logdir, "controls.npy")
+    X_PATH = os.path.join(logdir, "trajectories.npy")
     u = np.load(U_PATH)
     x = np.load(X_PATH)
 
-    params = json.load(open(os.path.join(LOGDIR, "params.json")))
+    vehicles_path = os.path.join(logdir, 'other_vehicles.p')
+    world_path = os.path.join(logdir, 'world.p')
+
+    vehicles = pickle.load(open(vehicles_path, 'rb'))
+    world = pickle.load(open(world_path, 'rb'))    
+
+    params = json.load(open(os.path.join(logdir, "params.json")))
+
+    return params, vehicles, world, x0, x, u
+
+
+def get_car_colors_from_svo(vehicles):
+    ''' Get car color string for each car based on the car's SVO'''
+    svo = []
+    for veh in vehicles:
+        if hasattr(veh, "theta_ij"):
+            svo += [veh.theta_ij[-1]]
+        else:
+            svo += [veh.theta_iamb]
+    
+    car_colors = ['r' for i in range(len(svo))]
+    for i in range(len(svo)):
+        if svo[i] < np.pi / 8.0:
+            car_colors[i] = 'red'
+        elif np.pi / 8 <= svo[i] <= 3 * np.pi / 8.0:
+            car_colors[i] = 'purple'
+        elif np.pi / 8 <= svo[i] <= np.pi / 2.0:
+            car_colors[i] = 'blue'
+    
+    return car_colors
+
+def animate_cars(args, params, log_directory, trajectories, vehicles, world):
+    ''' Create a video of the cars driving around'''
+
+
+    vids_path = glob.glob(log_directory + "/*/*.mp4")
+    if args.keep_old_vids and len(vids_path) > 0:
+        print("Video already exists: %s" % log_directory)
+        return None
+
+    if args.end_mpc != -1:
+        trajectories = trajectories[:, :, :args.end_mpc]
+
+    # Prep the image directory
+    img_dir = os.path.join(log_directory, "imgs")
+    print("Img dir", img_dir)
+    os.makedirs(img_dir, exist_ok=True)
+    filelist = glob.glob(os.path.join(img_dir, "*.png"))
+    for f in filelist:
+        os.remove(f)
+
+    # For when we would return a large vector that included datapoints defaulted to zero
+    # if xamb_actual.shape[1] > end_frame:
+    #     xamb_actual = xamb_actual[:, :end_frame]
+    #     xothers_actual = [x[:, :end_frame] for x in xothers_actual]
+
+    print("Saving photos to %s..." % img_dir)
+
+    if args.svo_colors == 1:
+        car_colors = get_car_colors_from_svo(vehicles)
+    else:
+        car_colors = None
+
+    plot_cars(world,
+              None,
+              None, [trajectories[i, :, :] for i in range(params["n_other"])],
+              img_dir + "/",
+              args.shape,
+              None,
+              None,
+              car_colors,
+              args.n_workers,
+              vid_track=args.vehicle_id_track,
+              all_other_vehicles=vehicles)
+
+    # log_string = log_directory.split('/')[-2]
+    log_string = log_directory
+    vid_dir = os.path.join(log_directory, 'vids/')
+    os.makedirs(vid_dir, exist_ok=True)
+    print("Video directory", vid_dir)
+    if args.shape == "both" or args.shape == "image":
+        video_filename = os.path.join(vid_dir, log_string + '.mp4')
+    else:
+        video_filename = os.path.join(vid_dir, log_string + 'ell.mp4')
+
+    if os.path.exists(video_filename):
+        os.remove(video_filename)
+    outfile = animate(img_dir, video_filename, args.fps)
+    print("Saved video to: %s" % outfile)
+    vid_directory = '/home/nbuckman/mpc-vids/'
+    cmd = 'cp %s %s' % (video_filename, vid_directory)
+    os.system(cmd)
+    print("Saved video to: %s" % vid_directory)
+
+    return None
+
+
+
+def analyze_controls(LOGDIR, u, x):    
+    os.makedirs(os.path.join(LOGDIR, "plots/"), exist_ok=True)
 
     np.set_printoptions(3, suppress=True)
     for ag_i in range(x.shape[0]):
@@ -27,7 +148,6 @@ def post_process(args):
 
     print(u[4, :, :])
 
-    os.makedirs(os.path.join(LOGDIR, "plots/"), exist_ok=True)
     plt.figure()
     plt.plot(u[4,0,:])
     plt.ylabel("u0_steering_change")
@@ -42,14 +162,27 @@ def post_process(args):
     return None
 
 
+
 if __name__ == '__main__':
 
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("logdir", type=str, help="log directory to analyze")
-    parser.add_argument("--analyze_controls", action="store_true", help="load controls")
+    parser.add_argument("logdir", type=str, nargs="+", help="log directory to analyze")
+    parser.add_argument("--show-initial-positions", action="store_true", help="load controls")
+    parser.add_argument("--analyze-controls", action="store_true", help="load controls")
+    parser.add_argument("--animate", action="store_true", help="Animate simulation")
+
+
+    parser.add_argument('--end-mpc', type=int, default=-1)
+    parser.add_argument('--vehicle-id-track', type=int, default=0)
+    parser.add_argument('--camera-speed', type=int, default=-1)
+    parser.add_argument('--shape', type=str, default="ellipse")
+    parser.add_argument('--svo-colors', type=int, default=1)
+    parser.add_argument('--n-workers', type=int, default=8)
+    parser.add_argument('--keep-old-vids', type=bool, default=False)
+    parser.add_argument('--fps', type=int, default=16, help="Frame rate in frames per second")
+
+
+
     args = parser.parse_args()
-
-
 
     post_process(args)
